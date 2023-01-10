cf5a2e1de41d575da4a812ce92f90b8710c39e75
GG <me@here.com>
2023-01-10 11:26:31 +0000

    Data Types 1st

---
+++ Eclipse/Demo App/src/com/DataTypes.java
@@ -0,0 +1,11 @@
+package com;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int a;
+		int b=100;
+		System.out.println(b);
+		System.out.println("Value of b is "+b);
+	}
+}
