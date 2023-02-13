5f6df82734e2dda8be4133b834bb401c6f2e70e3
giannis <gianniszouliasv@ip-172-31-21-252.ec2.internal>
2023-02-13 13:53:16 +0000

    Database Connection

---
+++ Course 2/com/src/com/DemoTest.java
@@ -0,0 +1,32 @@
+package com;
+import java.sql.*;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			//Class.forName("com.mysql.jdbc.Driver");	// MySQL 5.x
+			Class.forName("com.mysql.cj.jdbc.Driver");	// MySQL 8.x
+			System.out.println("Driver loaded successfully!");
+			
+			Connection con = DriverManager.
+					getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Simplilearn");
+			System.out.println("Database connected successfully!");
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from employee");
+			
+			while (rs.next()) {
+				System.out.println
+				("id is "+rs.getInt(1)+" Name is " + rs.getString(2)+ " Salary is " +rs.getFloat(3));
+			}
+			
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
