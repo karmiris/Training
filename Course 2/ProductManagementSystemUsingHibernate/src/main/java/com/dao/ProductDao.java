b3217bfc4099e07ca22a8ae6f5e5dd3d5d7f1ff1
giannis <giannis.zoulias@vodafone.com>
2023-02-17 09:46:38 +0000

    Product DAO

--- Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/dao/ProductDao.java
+++ Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/dao/ProductDao.java
@@ -3,7 +3,6 @@ package com.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
 import com.entity.Product;
 import com.resource.DbResource;
 
@@ -11,14 +10,14 @@ public class ProductDao {
 
 	public int storeProduct(Product product) {
 		try {
-		SessionFactory sf = DbResource.getSessionFactory();
-		Session session = sf.openSession();
-		Transaction tran = session.getTransaction();
-		tran.begin();
+			SessionFactory sf = DbResource.getSessionFactory();
+			Session session = sf.openSession();
+			Transaction tran = session.getTransaction();
+			tran.begin();
 			session.save(product);
-		tran.commit();
-		return 1;
-		}catch(Exception e) {
+			tran.commit();
+			return 1;
+		} catch(Exception e) {
 			System.out.println(e);
 			return 0;
 		}
