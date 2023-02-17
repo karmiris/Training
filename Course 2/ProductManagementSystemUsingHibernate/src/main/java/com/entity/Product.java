aaf0be1f1cf9e3d9dd7a42dd5b5d7fa79d08058d
karmiris <79927820+karmiris@users.noreply.github.com>
2023-02-17 11:45:25 +0200

    Update Product.java

--- Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/entity/Product.java
+++ Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/entity/Product.java
@@ -1,41 +1,35 @@
 package com.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Column;
 
-@Entity	// mandatory
-@Table (name = "employee")	// case insensitive; mandatory if different
+@Entity
 public class Product {
-	@Id 	// column with pk (primary key); mandatory
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
-	@Column (name = "id")	// case insensitive; mandatory if different
-	private int id;
-	@Column (name = "name")
-	private String name;
-	@Column (name = "salary")
-	private float salary;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public float getSalary() {
-		return salary;
-	}
-	public void setSalary(float salary) {
-		this.salary = salary;
-	}
-	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)	// auto_increment 
+private int pid;
+private String pname;
+private float price;
+public int getPid() {
+	return pid;
+}
+public void setPid(int pid) {
+	this.pid = pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+
+
 }
