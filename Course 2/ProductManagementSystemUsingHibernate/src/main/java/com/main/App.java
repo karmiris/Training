221e730ed84db5a63cc8ee9fa9ab1c7ae543de2f
karmiris <79927820+karmiris@users.noreply.github.com>
2023-02-17 11:59:07 +0200

    Update App.java

--- Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/main/App.java
+++ Course 2/ProductManagementSystemUsingHibernate/src/main/java/com/main/App.java
@@ -1,15 +1,17 @@
 package com.main;
 
+import com.entity.Product;
+import com.service.ProductService;
 
 public class App {
 
 	public static void main(String[] args) {
-		try {
-							
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+		ProductService ps = new ProductService();
+		Product p = new Product();
+		p.setPname("Computer");
+		p.setPrice(45000);
+		String result = ps.storeProduct(p);
+		System.out.println(result);
 
 	}
 
